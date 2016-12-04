a64e03b37854d5540b91cfa3640f39b0115e45c5
mzks <mz@stu.kobe-u.ac.jp>
2016-12-04 19:28:13 +0900

    Construct Envelope 22Na source Case

--- source/src/Geometry.cc
+++ source/src/Geometry.cc
@@ -17,147 +17,208 @@
 #include "G4Element.hh"
 
 //------------------------------------------------------------------------------
-  Geometry::Geometry() {}
+Geometry::Geometry() {}
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-  Geometry::~Geometry() {}
+Geometry::~Geometry() {}
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-  G4VPhysicalVolume* Geometry::Construct()
-//------------------------------------------------------------------------------
+G4VPhysicalVolume* Geometry::Construct()
+	//------------------------------------------------------------------------------
 {
-// Get pointer to 'Material Manager'
-   G4NistManager* materi_Man = G4NistManager::Instance();
-// Define materials
-   G4double density;
-   G4int ncomponents;
-   G4int natoms;
-   G4double fractionmass;
-   G4double z,a;
-   G4String symbol;
+	// Get pointer to 'Material Manager'
+	G4NistManager* materi_Man = G4NistManager::Instance();
 
-   G4Element* Na = new G4Element("Sodium"   ,"Na", z=11., a=  22.99*g/mole);
-   G4Element* I  = new G4Element("Iodine"   ,"I" , z=53., a= 126.90*g/mole);
-   G4Element* Si = new G4Element("Scilicon",symbol="Si" , z= 14., a= 28.09*g/mole);
-   G4Element* O  = new G4Element("Oxygen"  ,symbol="O" , z= 8., a= 16.00*g/mole);
-   G4Element* H  = new G4Element("Hydrogen",symbol="H" , z= 1., a= 1.01*g/mole);
+	// Define materials
+	G4double density;
+	G4int ncomponents;
+	G4int natoms;
+	G4double fractionmass;
+	G4double z,a;
+	G4String symbol;
 
-   Al = new G4Material("Aluminum", z=13., a=26.98*g/mole, density=2.700*g/cm3);
+	G4Element* Na = new G4Element("Sodium"   ,"Na", z=11., a=  22.99*g/mole);
+	G4Element* I  = new G4Element("Iodine"   ,"I" , z=53., a= 126.90*g/mole);
+	G4Element* Si = new G4Element("Scilicon",symbol="Si" , z= 14., a= 28.09*g/mole);
+	G4Element* O  = new G4Element("Oxygen"  ,symbol="O" , z= 8., a= 16.00*g/mole);
+	G4Element* C  = new G4Element("Carbon"  ,symbol="C" , z= 6., a= 12.01*g/mole);
+	G4Element* H  = new G4Element("Hydrogen",symbol="H" , z= 1., a= 1.01*g/mole);
 
-   Sci = new G4Material("Scintillator", density= 1.032*g/cm3, ncomponents=2);
-   Sci->AddElement(C, natoms=9);
-   Sci->AddElement(H, natoms=10);
+	G4Material* Al = new G4Material("Aluminum", z=13., a=26.98*g/mole, density=2.700*g/cm3);
 
-   SilicaGel = new G4Material("SilicaGel", density= 0.55*g/cm3, ncomponents=2);
-   SilicaGel->AddElement(Si, natoms=1);
-   SilicaGel->AddElement(O, natoms= 2);
+	G4Material* Sci = new G4Material("Scintillator", density= 1.032*g/cm3, ncomponents=2);
+	Sci->AddElement(C, natoms=9);
+	Sci->AddElement(H, natoms=10);
 
-   // Define 'World Volume'
-   // Define the shape of solid
-   
-   G4double leng_X_World = 2.0*m;         // X-full-length of world 
-   G4double leng_Y_World = 2.0*m;         // Y-full-length of world 
-   G4double leng_Z_World = 2.0*m;         // Z-full-length of world 
+	G4Material* SilicaGel = new G4Material("SilicaGel", density= 0.55*g/cm3, ncomponents=2);
+	SilicaGel->AddElement(Si, natoms=1);
+	SilicaGel->AddElement(O, natoms= 2);
 
-   G4Box* solid_World = 
-	   new G4Box("Solid_World", leng_X_World/2.0, leng_Y_World/2.0, leng_Z_World/2.0);
+	G4Material* NaI = new G4Material("NaI", density= 3.67*g/cm3, ncomponents=2);
+	NaI->AddElement(Na, natoms=1);
+	NaI->AddElement(I , natoms=1);
+	NaI->GetIonisation()->SetMeanExcitationEnergy(452*eV);
 
-   // Define logical volume
-   G4Material* materi_World = materi_Man->FindOrBuildMaterial("G4_AIR");
-   G4LogicalVolume* logVol_World = 
-	   new G4LogicalVolume(solid_World, materi_World, "LogVol_World");
-   logVol_World->SetVisAttributes (G4VisAttributes::Invisible);
+	// Define 'World Volume'
+	// Define the shape of solid
 
-   // Placement of logical volume
-   G4int copyNum_World = 0;               // Set ID number of world
-   G4PVPlacement* physVol_World  = 
-	   new G4PVPlacement(G4Transform3D(), "PhysVol_World", logVol_World, 0, 
-			   false, copyNum_World);
+	G4double leng_X_World = 2.0*m;         // X-full-length of world 
+	G4double leng_Y_World = 2.0*m;         // Y-full-length of world 
+	G4double leng_Z_World = 2.0*m;         // Z-full-length of world 
 
-   //NaI
-   G4double NaILength = 50.0* mm;
-   G4double NaIDiameter = 50.0 * mm;
+	G4Box* solid_World = 
+		new G4Box("Solid_World", leng_X_World/2.0, leng_Y_World/2.0, leng_Z_World/2.0);
 
-   G4Tubs* solidNaI = new G4Tubs(
-		   "solidNaI",//name
-		   0,//Inner radius
-		   NaILength / 2.0,//outer radius
-		   NaIDiameter / 2.0,//half length in Z
-		   0,//the starting phi angle in radians
-		   CLHEP::twopi//the angle of the segment in radians
-		   );
-   G4Tubs* solid22NaCase = new G4Tubs("solid22NaCase",
-		   0,
-		   10.0 * mm,
-		   3.0 * mm,
-		   0.0,
-		   CLHEP::twopi);
+	// Define logical volume
+	G4Material* materi_World = materi_Man->FindOrBuildMaterial("G4_AIR");
+	G4LogicalVolume* logVol_World = 
+		new G4LogicalVolume(solid_World, materi_World, "LogVol_World");
+	logVol_World->SetVisAttributes (G4VisAttributes::Invisible);
+
+	// Placement of logical volume
+	G4int copyNum_World = 0;               // Set ID number of world
+
+	G4PVPlacement* physVol_World  = new G4PVPlacement(
+				G4Transform3D(),
+			   	"PhysVol_World",
+			   	logVol_World,	
+			   	0, 
+				false,
+			   	copyNum_World);
+
+	//Parameters
+	G4Tubs* env22NaCase = new G4Tubs(
+			"env22NaCase",
+			0,
+			25.0/2.0 * mm,
+			5.0 / 2.0 * mm,
+			0,
+			CLHEP::twopi);
+
+	G4Tubs* solid22NaCase = new G4Tubs(
+			"solid22NaCase",
+			8.0 /2.0 * mm,
+			25.0/2.0 * mm,
+			5.0 / 2.0 * mm,
+			0.0,
+			CLHEP::twopi);
+
+	G4Tubs* solid22NaCaseBottom = new G4Tubs(
+			"solid22NaCaseBottom",
+			0,
+			8.0/2.0 * mm,
+			4.0 / 2.0 * mm,
+			0.0,
+			CLHEP::twopi);
+
+	G4Tubs* solid22NaCaseWindow = new G4Tubs(
+			"solid22NaCaseWindow",
+			0,
+			8.0/2.0 * mm,
+			1 / 2.0 * um,
+			0.0,
+			CLHEP::twopi);
+	
+	G4LogicalVolume* logenv22NaCase = new G4LogicalVolume(
+			env22NaCase,
+			Sci,
+			"logenv22NaCase",
+			0,0,0,true);
+	G4LogicalVolume* logSide22NaCase = new G4LogicalVolume(
+			solid22NaCase,
+			Sci,
+			"logSide22NaCase",
+			0,0,0,true);
+	G4LogicalVolume* logBottom22NaCase = new G4LogicalVolume(
+			solid22NaCaseBottom,
+			Sci,
+			"logBottom22NaCase",
+			0,0,0,true);
+	G4LogicalVolume* logWindow22NaCase = new G4LogicalVolume(
+			solid22NaCaseWindow,
+			Al,
+			"logWindow22NaCase",
+			0,0,0,true);
+
+	new G4PVPlacement( 
+			G4Transform3D(), //rotation and vector
+			logSide22NaCase,	//logical volume
+			"physEnv22NaCaseSide",//name
+			logenv22NaCase, //mother logicall volume
+			false,				//set to false
+			9001,			//copy number
+			true);			// check
+
+	new G4PVPlacement(
+			G4Transform3D(),
+			"sourceCase",
+			logenv22NaCase,
+		   	physVol_World, 
+			false,
+			9002,
+			true);
+			
+			
+	
+	//NaI
+	G4double NaILength = 50.0* mm;
+	G4double NaIDiameter = 50.0 * mm;
+
+	G4Tubs* solidNaI = new G4Tubs(
+			"solidNaI",//name
+			0,//Inner radius
+			NaILength / 2.0,//outer radius
+			NaIDiameter / 2.0,//half length in Z
+			0,//the starting phi angle in radians
+			CLHEP::twopi//the angle of the segment in radians
+			);
 
 
-   G4LogicalVolume* logicNa22 = new G4LogicalVolume(solidNa22,	
-		   Sci,	
-		   "Na22");
+	G4LogicalVolume* logNaI = new G4LogicalVolume(solidNaI,NaI,"logNaI",0,0,0);
 
-  G4VisAttributes* na22VisAtt = new G4VisAttributes(G4Colour(0.5,0.5,0.2));
-  na22VisAtt->SetVisibility(true);
-  logicNa22->SetVisAttributes(na22VisAtt);
+	G4double pos_X_LogV = 0.0*cm;           // X-location LogV 
+	G4double pos_Y_LogV = 0.0*cm;           // Y-location LogV
+	G4double pos_Z_LogV = 10.0*cm;           // Z-location LogV
 
- 
-  new G4PVPlacement(0,		
-		    G4ThreeVector(0.,0.,param.fNa22Z),
-		    logicNa22,		
-		    "Na22",		
-		    logicWorld,			
-		    false,			
-		    0);			
+	G4ThreeVector threeVect_LogV = G4ThreeVector(pos_X_LogV, pos_Y_LogV, pos_Z_LogV);
+	G4RotationMatrix rotMtrx_LogV = G4RotationMatrix();
+	G4Transform3D trans3D_LogV = G4Transform3D(rotMtrx_LogV, threeVect_LogV);
 
+	G4int copyNum_LogV = 1000;                // Set ID number of LogV
+	new G4PVPlacement(
+			trans3D_LogV,
+		   	"PhysVol_NaI",
+		   	logNaI,
+		   	physVol_World, 
+			false,
+		   	copyNum_LogV
+			,true);
 
-
-   G4Material* NaI = 
-	   new G4Material("NaI", density= 3.67*g/cm3, ncomponents=2);
-   NaI->AddElement(Na, natoms=1);
-   NaI->AddElement(I , natoms=1);
-   NaI->GetIonisation()->SetMeanExcitationEnergy(452*eV);
-   
-   G4LogicalVolume* logNaI = new G4LogicalVolume(solidNaI,NaI,"logNaI",0,0,0);
-
-   G4double pos_X_LogV = 0.0*cm;           // X-location LogV 
-   G4double pos_Y_LogV = 0.0*cm;           // Y-location LogV
-   G4double pos_Z_LogV = 10.0*cm;           // Z-location LogV
-
-   G4ThreeVector threeVect_LogV = G4ThreeVector(pos_X_LogV, pos_Y_LogV, pos_Z_LogV);
-   G4RotationMatrix rotMtrx_LogV = G4RotationMatrix();
-   G4Transform3D trans3D_LogV = G4Transform3D(rotMtrx_LogV, threeVect_LogV);
-
-   G4int copyNum_LogV = 1000;                // Set ID number of LogV
-   new G4PVPlacement(trans3D_LogV, "PhysVol_NaI", logNaI, physVol_World, 
-                     false, copyNum_LogV,true);
-
-   threeVect_LogV.rotateY(45.0 * deg);
-   rotMtrx_LogV.rotateY(45.0 * deg);
-   copyNum_LogV = 2000;                // Set ID number of LogV
-   new G4PVPlacement(G4Transform3D(rotMtrx_LogV,threeVect_LogV), "PhysVol_NaI", logNaI, physVol_World, 
-                     false, copyNum_LogV,true);
-   threeVect_LogV.rotateY(45.0 * deg);
-   rotMtrx_LogV.rotateY(45.0 * deg);
-   copyNum_LogV = 3000;                // Set ID number of LogV
-   new G4PVPlacement(G4Transform3D(rotMtrx_LogV,threeVect_LogV), "PhysVol_NaI", logNaI, physVol_World, 
-                     false, copyNum_LogV,true);
-   threeVect_LogV.rotateY(45.0 * deg);
-   rotMtrx_LogV.rotateY(45.0 * deg);
-   copyNum_LogV = 4000;                // Set ID number of LogV
-   new G4PVPlacement(G4Transform3D(rotMtrx_LogV,threeVect_LogV), "PhysVol_NaI", logNaI, physVol_World, 
-                     false, copyNum_LogV,true);
-   threeVect_LogV.rotateY(45.0 * deg);
-   rotMtrx_LogV.rotateY(45.0 * deg);
-   copyNum_LogV = 5000;                // Set ID number of LogV
-   new G4PVPlacement(G4Transform3D(rotMtrx_LogV,threeVect_LogV), "PhysVol_NaI", logNaI, physVol_World, 
-                     false, copyNum_LogV,true);
+	threeVect_LogV.rotateY(45.0 * deg);
+	rotMtrx_LogV.rotateY(45.0 * deg);
+	copyNum_LogV = 2000;                // Set ID number of LogV
+	new G4PVPlacement(G4Transform3D(rotMtrx_LogV,threeVect_LogV), "PhysVol_NaI", logNaI, physVol_World, 
+			false, copyNum_LogV,true);
+	threeVect_LogV.rotateY(45.0 * deg);
+	rotMtrx_LogV.rotateY(45.0 * deg);
+	copyNum_LogV = 3000;                // Set ID number of LogV
+	new G4PVPlacement(G4Transform3D(rotMtrx_LogV,threeVect_LogV), "PhysVol_NaI", logNaI, physVol_World, 
+			false, copyNum_LogV,true);
+	threeVect_LogV.rotateY(45.0 * deg);
+	rotMtrx_LogV.rotateY(45.0 * deg);
+	copyNum_LogV = 4000;                // Set ID number of LogV
+	new G4PVPlacement(G4Transform3D(rotMtrx_LogV,threeVect_LogV), "PhysVol_NaI", logNaI, physVol_World, 
+			false, copyNum_LogV,true);
+	threeVect_LogV.rotateY(45.0 * deg);
+	rotMtrx_LogV.rotateY(45.0 * deg);
+	copyNum_LogV = 5000;                // Set ID number of LogV
+	new G4PVPlacement(G4Transform3D(rotMtrx_LogV,threeVect_LogV), "PhysVol_NaI", logNaI, physVol_World, 
+			false, copyNum_LogV,true);
 
 
 
-// Return the physical world
-   return physVol_World;
+	// Return the physical world
+	return physVol_World;
 }
